3be42f42015bc49e8690507be1f82ce6af4b71ee
Santamaria12 <medicenjonzalex@gmail.com>
2024-07-30 15:59:04 -0600

    Update database.js

--- database.js
+++ database.js
@@ -6,3 +6,8 @@ db.serialize(() => {
 });
 
 module.exports = db;
+
+db.serialize(() => {
+    db.run("CREATE TABLE IF NOT EXISTS users (id TEXT PRIMARY KEY, balance INTEGER, rank TEXT, role TEXT)");
+    db.run("INSERT OR IGNORE INTO users (id, balance, rank, role) VALUES ('+50581261007@c.us', 10000, 'Rey', 'Rey')");
+});
